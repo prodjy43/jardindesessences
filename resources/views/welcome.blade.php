754728691136d08f67610e95409e51355727e623
prodjy43 <prodjy43@gmail.com>
2016-11-04 15:05:17 +0100

    design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <title>Jardin des essences</title>
-</head>
-<body>
-
-</body>
+    <link rel="stylesheet" href="{{ elixir('css/unsemantic.css') }}">
+    <link rel="stylesheet" href="{{ elixir('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <script src="{{ elixir('js/jquery.min.js') }}"></script>
+    <script src="{{ elixir('js/app.js') }}"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta viewport="">
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Préstations</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="banner">
+      <div class="opacity">
+        <h1>Au jardin des essences</h1>
+        <h2>Salon de massage ayurveda</h2>
+      </div>
+    </div>
+  </body>
 </html>
